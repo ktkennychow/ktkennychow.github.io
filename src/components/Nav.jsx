86a2e6fe3b33aa229448f120a4599ad2a4afb8b8
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-10-01 14:55:30 -0400

    remove use of gsap

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,10 +4,10 @@ const Nav = () => {
   const [currentPage, setCurrentPage] = React.useState('about')
 
   return (
-    <div>
+    <>
       <nav className='sticky flex flex-col top-0 text-white h-screen items-center z-10'>
-        <div className='sticky top-0 top-line w-1  flex-auto bg-white'></div>
-        <ul className='sticky top-1/2 right-0 my-4 -translate-y-1/2'>
+        <div className='abosolute top-0 w-1 flex-auto bg-white'></div>
+        <ul className='abosolute top-1/2 right-0 my-4'>
           <li
             className={`about transition hover:-translate-x-4 duration-500 ease-in-out cursor-pointer text-xs py-2.5 px-2 md:text-base md:px-5 text-center ${
               currentPage === 'about'
@@ -57,9 +57,9 @@ const Nav = () => {
             Contact
           </li>
         </ul>
-        <div className='sticky bottom-0 bottom-line w-1 flex-auto bg-white '></div>
+        <div className='abosolute bottom-0 w-1 flex-auto bg-white '></div>
       </nav>
-    </div>
+    </>
   )
 }
 
