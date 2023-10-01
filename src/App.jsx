c0054891d8f200a1163af9d4f3c9595ba8915b44
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-10-01 15:37:49 -0400

    change height to 100svh

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from './components/Contact'
 
 const App = () => {
   return (
-    <div className='z-10 bg-zinc-900 text-stone-300 flex h-full max-w-full font-poppins font-light overscroll-none justify-between'>
+    <div className='relative bg-zinc-900 z-10 text-stone-300 flex h-[100svh] w-full max-w-full font-poppins font-light overscroll-none justify-between overflow-scroll'>
       <div
         id='main'
         className='flex flex-col gap-32 ml-5 md:ml-10'>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,8 +6,8 @@ const Nav = () => {
   return (
     <>
       <nav className='sticky flex flex-col top-0 text-white h-screen items-center z-10'>
-        <div className='abosolute top-0 w-1 flex-auto bg-white'></div>
-        <ul className='abosolute top-1/2 right-0 my-4'>
+        <div className='top-0 w-1 flex-auto bg-white'></div>
+        <ul className='top-1/2 right-0 my-4'>
           <li
             className={`about transition hover:-translate-x-4 duration-500 ease-in-out cursor-pointer text-xs py-2.5 px-2 md:text-base md:px-5 text-center ${
               currentPage === 'about'
@@ -57,7 +57,7 @@ const Nav = () => {
             Contact
           </li>
         </ul>
-        <div className='abosolute bottom-0 w-1 flex-auto bg-white '></div>
+        <div className='bottom-0 w-1 flex-auto bg-white '></div>
       </nav>
     </>
   )
