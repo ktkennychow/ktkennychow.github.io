c0ba16cd666c7e7caa3ea31bd0e0a740c5966e23
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-10-04 14:01:49 -0400

    self-hosting fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./styles/tailwind.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles/tailwind.css';
+import '@fontsource/poppins/200.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource-variable/montserrat';
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	darkMode: "class",
-	theme: {
-		extend: {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
       fontFamily: {
-				'montserrat': ["Montserrat", "serif"],
-				'poppins': ["Poppins", "serif"],
-			}
+        montserrat: ['Montserrat Variable', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
     },
-	},
-	plugins: [],
+  },
+  plugins: [],
 };
