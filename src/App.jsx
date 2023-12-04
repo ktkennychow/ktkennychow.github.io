5f62ba097d0cad3f095e298e6068b822010b2868
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-12-04 12:14:40 -0500

    chore: update contact infomation

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Projects from './components/Projects';
 
 const App = () => {
   return (
-    <div className='relative flex h-[100svh] w-full max-w-full justify-between overflow-x-hidden overscroll-none bg-zinc-900 font-poppins font-light leading-relaxed text-white text-opacity-80 '>
+    <div className='relative flex h-[100svh] w-full max-w-full justify-between overflow-x-hidden overscroll-none bg-zinc-900 font-poppins font-light leading-relaxed text-white text-opacity-80'>
       <div id='main' className='mx-auto flex w-full flex-col gap-16 p-10 md:max-w-3xl'>
         <Intro />
         <Projects />
