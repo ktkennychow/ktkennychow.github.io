734056efd56d068509731aaf2d642932d04a4fe1
ktkennychow <ktkennychow@gmail.com>
2025-10-17 11:04:03 +0900

    chore: update dependencies and improve component styles

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,42 +5,29 @@ import books from '../data/books';
 const Intro = () => {
   return (
     <div id='about' className='flex max-w-3xl flex-col gap-6'>
-      <div className='flex flex-1 items-center gap-6'>
-        <div className='w-32 h-32 bg-black rounded-full flex items-center justify-center'>
+      <div className='flex flex-1 items-center gap-4'>
+        <div className='min-w-32 min-h-32 w-32 h-32 bg-black rounded-lg flex items-center justify-center'>
           <img
             src={profilePic}
             alt='Profile Picture of Kenny Chow'
             className='w-28 rounded-full'
             />
         </div>
-        <div className='flex flex-1 flex-col'>
+        <div className='flex flex-1 flex-col gap-1'>
           <h1 className='font-montserrat text-2xl font-bold text-white'>Kenny Chow</h1>
           <h2 className='flex-1 text-sm'> Software Development | Product</h2>
           <Contact />
+          <p className='text-sm'>Climbing, reading and listening to podcasts in my free time.</p>
         </div>
-      </div>
-      <div className=''>
-        <p>Climbing, reading and listening to podcasts in my free time.</p>
       </div>
       <div className='flex flex-col gap-2'>
         <div className='flex flex-col'>
-        <h3 className='text-md font-semibold'>BookShelf</h3>
-        <p className='text-xs'>Only books I have read and will consider reading again.</p>
+          <h3 className='text-lg font-semibold'>BookShelf</h3>
+          <p className='text-xs'>Only books I have read and will consider reading again.</p>
         </div>
         {books.sort((a, b) => a.title.localeCompare(b.title)).map((book) => (
-          <div key={book.title} className='flex gap-1'>
-            <div key={book.title} className='flex gap-1'>
-              <a
-                href={book.href}
-                target='_blank'
-                rel='noopener noreferrer'
-                className='text-sm underline underline-offset-4'>
-                {book.title}
-              </a>
-              <svg width='8' height='8' viewBox='0 0 8 8' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <path id='Vector 6' d='M1 1H7M7 1V7M7 1L1 7' stroke='white' />
-              </svg>
-            </div>
+          <div key={book.title} className='flex flex-col gap-0'>
+            <p className='text-md'>{book.title}</p>
             <p className='text-xs'>by {book.author}</p>
           </div>
         ))}
--- src/data/books.js
+++ src/data/books.js
@@ -2,51 +2,41 @@ export default [
   {
     title: 'Factfulness',
     author: 'Hans Rosling',
-    href: 'https://www.goodreads.com/book/show/34890015',
   },
   {
     title: 'On Writing Well',
     author: 'William Zinsser',
-    href: 'https://www.goodreads.com/book/show/53343',
   },
   {
     title: 'How to Solve it',
     author: 'George Polya',
-    href: 'https://www.goodreads.com/book/show/192221',
   },
   {
     title: 'The Psychology of Money',
     author: 'Morgan Housel',
-    href: 'https://www.goodreads.com/book/show/41881472',
   },
   {
     title: 'Discourses, Fragments, and Letters',
     author: 'Epictetus & Robin Hard',
-    href: 'https://www.goodreads.com/book/show/18189134',
   },
   {
     title: 'Hell Yeah or No',
     author: 'Derek Sivers',
-    href: 'https://www.goodreads.com/book/show/52523856',
   },
   {
-    title: 'How to Live: 27 conflicting answers and one weird conclusion',
+    title: 'How to Live',
     author: 'Derek Sivers',
-    href: 'https://www.goodreads.com/book/show/58188742',
   },
   {
     title: 'Anything You Want',
     author: 'Derek Sivers',
-    href: 'https://www.goodreads.com/book/show/11878168',
   },
   {
     title: 'The Courage to Be Disliked',
     author: 'Ichiro Kishimi & Fumitake Koga',
-    href: 'https://www.goodreads.com/book/show/43306206',
   },
   {
     title: 'Sapiens: A Brief History of Humankind',
     author: 'Yuval Noah Harari',
-    href: 'https://www.goodreads.com/book/show/23692271',
   },
 ];
